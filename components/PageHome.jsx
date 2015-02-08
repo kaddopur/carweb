fea55fb69cc2487a2c1892af47d778880b63404a
Jason Huang <cjhuang@yahoo-inc.com>
2015-02-09 01:12:38 +0800

    add carousel nav button

--- components/PageHome.jsx
+++ components/PageHome.jsx
@@ -8,20 +8,21 @@ var PageHome = React.createClass({
     getInitialState: function () {
         return {
             slides: [
-                'SVgkNW2cENQ', 'q6f-LLM1H6U'
+                'SVgkNW2cENQ', 'q6f-LLM1H6U', 'CWFB8HCQsu0'
             ],
             currentSlide: 0
         };
     },
-    componentDidMount: function() {
-        this.timer = setInterval(this.nextSlide, 5000);
-    },
-    componentWillUnmount: function() {
-        clearInterval(this.timer);
-    },
-    nextSlide: function () {
+    nextSlide: function (step) {
+        if (this.state.currentSlide === 0 && step === -1) {
+            return;
+        } else if (this.state.currentSlide === this.state.slides.length - 1 && step === 1) {
+            return;
+        }
+
         this.setState({
-            currentSlide: (this.state.currentSlide + 1) % this.state.slides.length
+            currentSlide: (this.state.currentSlide + this.state.slides.length + step) % this.state.slides.length,
+            transitionName: (step === 1 ? 'carousel' : 'carousel-reverse')
         });
     },
     render: function() {
@@ -45,13 +46,15 @@ var PageHome = React.createClass({
         return (
             <div className="pure-g">
                 <div className="pure-u-1 pure-u-sm-3-5 pure-u-lg-1-2">
-                    <HeroVideo yid="CWFB8HCQsu0"/>
+                    <HeroVideo yid="JqNCz7jyK50"/>
                 </div>
                 <div className="pure-u-1 pure-u-sm-2-5 pure-u-lg-1-3 carousel-container">
-                    <ReactCSSTransitionGroup transitionName="carousel">
+                    <ReactCSSTransitionGroup transitionName={this.state.transitionName}>
                         {slides[this.state.currentSlide]}
                     </ReactCSSTransitionGroup>
                 </div>
+                <button onClick={this.nextSlide.bind(null, -1)}>Prev</button>
+                <button onClick={this.nextSlide.bind(null, 1)}>Next</button>
             </div>
         );
     }
