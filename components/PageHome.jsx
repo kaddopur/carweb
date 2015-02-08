b951b68c7d15cf21ffe189d4baf7ebfef3f35464
Jason Huang <cjhuang@yahoo-inc.com>
2015-02-09 00:30:16 +0800

    add carousel animation

--- components/PageHome.jsx
+++ components/PageHome.jsx
@@ -1,27 +1,56 @@
 'use strict';
-var React = require('react');
+var React = require('react/addons');
 var HeroVideo = require('./HeroVideo');
 var CoverTile = require('./CoverTile');
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 var PageHome = React.createClass({
     getInitialState: function () {
-        return {};
+        return {
+            slides: [
+                'SVgkNW2cENQ', 'q6f-LLM1H6U'
+            ],
+            currentSlide: 0
+        };
+    },
+    componentDidMount: function() {
+        this.timer = setInterval(this.nextSlide, 5000);
+    },
+    componentWillUnmount: function() {
+        clearInterval(this.timer);
+    },
+    nextSlide: function () {
+        this.setState({
+            currentSlide: (this.state.currentSlide + 1) % this.state.slides.length
+        });
     },
     render: function() {
+        var slides = this.state.slides.map(function (slide, index) {
+            return (
+                <div className="pure-g" key={index}>
+                    <div className="pure-u-1-2">
+                        <CoverTile yid={slide}/>
+                        <CoverTile yid={slide}/>
+                        <CoverTile yid={slide}/>
+                    </div>
+                    <div className="pure-u-1-2">
+                        <CoverTile yid={slide}/>
+                        <CoverTile yid={slide}/>
+                        <CoverTile yid={slide}/>
+                    </div>
+                </div>
+            );
+        });
+
         return (
             <div className="pure-g">
                 <div className="pure-u-1 pure-u-sm-3-5 pure-u-lg-1-2">
-                    <HeroVideo yid="SVgkNW2cENQ"/>
+                    <HeroVideo yid="CWFB8HCQsu0"/>
                 </div>
-                <div className="pure-u-1-2 pure-u-sm-1-5 pure-u-lg-1-6">
-                    <CoverTile yid="SVgkNW2cENQ"/>
-                    <CoverTile yid="q6f-LLM1H6U"/>
-                    <CoverTile yid="SVgkNW2cENQ"/>
-                </div>
-                <div className="pure-u-1-2 pure-u-sm-1-5 pure-u-lg-1-6">
-                    <CoverTile yid="SVgkNW2cENQ"/>
-                    <CoverTile yid="q6f-LLM1H6U"/>
-                    <CoverTile yid="SVgkNW2cENQ"/>
+                <div className="pure-u-1 pure-u-sm-2-5 pure-u-lg-1-3 carousel-container">
+                    <ReactCSSTransitionGroup transitionName="carousel">
+                        {slides[this.state.currentSlide]}
+                    </ReactCSSTransitionGroup>
                 </div>
             </div>
         );
