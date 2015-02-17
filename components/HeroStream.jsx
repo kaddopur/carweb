df60aca3c3e1172340798ec121152865d34f6a84
Jason Huang <cjhuang@yahoo-inc.com>
2015-02-17 16:01:29 +0800

    reactify HeroStream

--- components/HeroStream.jsx
+++ components/HeroStream.jsx
@@ -5,7 +5,34 @@ var React = require('react');
 var HeroStream = React.createClass({
     render: function() {
         return (
-            <div>Hero</div>
+            <div className="HeroStream">
+                <h2 className="StreamTitle">Now Watching</h2>
+                <div className="HeroVideo">
+                    <img src="http://placehold.it/560x315" alt=""/>
+                </div>
+                <div className="HeroSlide">
+                    <a href="#">
+                        <div className="CoverTile active">
+                            <label className="WatchingLabel">Now Watching</label>
+                            <label className="DurationLabel">1:19</label>
+                            <div className="CoverOverlay">
+                                <span>1,300 views</span>
+                                <p>Thoughts You Have While Thoughts You Have While</p>
+                            </div>
+                            <img src="http://placehold.it/320x180"/>
+                        </div>
+                    </a>
+                    <div className="CoverTile"><img src="http://placehold.it/320x180"/></div>
+                    <div className="CoverTile"><img src="http://placehold.it/320x180"/></div>
+                    <div className="CoverTile"><img src="http://placehold.it/320x180"/></div>
+                    <div className="CoverTile"><img src="http://placehold.it/320x180"/></div>
+                    <div className="CoverTile"><img src="http://placehold.it/320x180"/></div>
+                </div>
+                <div className="HeroNavButtonContainer">
+                    <span className="NavButton Left"></span>
+                    <span className="NavButton Right"></span>
+                </div>
+            </div>
         );
     }
 });
