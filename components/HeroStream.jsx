8a7d745c4fb7c26e2dbe8a45b9d488831b199042
Jason Huang <cjhuang@yahoo-inc.com>
2015-02-17 18:26:18 +0800

    change slide base on state

--- components/HeroStream.jsx
+++ components/HeroStream.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
-var React = require('react');
+var React = require('react/addons');
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 var HeroVideo = require('./HeroVideo');
 var HeroSlide = require('./HeroSlide');
 
@@ -53,26 +54,67 @@ var HeroStream = React.createClass({
                         youtubeId: '4JipHEz53sU'
                     }
 
+                ],
+                [
+                    {
+                        youtubeId: 'dFf4AgBNR1E'
+                    },
+                    {
+                        youtubeId: 'dFf4AgBNR1E'
+                    },
+                    {
+                        youtubeId: 'dFf4AgBNR1E'
+                    },
+                    {
+                        youtubeId: 'dFf4AgBNR1E'
+                    },
+                    {
+                        youtubeId: 'dFf4AgBNR1E'
+                    },
+                    {
+                        youtubeId: 'dFf4AgBNR1E'
+                    }
+
                 ]
             ],
             initSlideIndex: 0
         };
     },
-    render: function() {
-        var heroSlides = this.props.slides.map(function renderHeroSlide(slide, index) {
-            return (
-                <HeroSlide videos={slide} key={index}/>
-            );
+    getInitialState: function() {
+        return {
+            slideIndex: this.props.initSlideIndex
+        };
+    },
+    handleIndexChange: function(step) {
+        var slideLength = this.props.slides.length;
+
+        this.setState({
+            slideIndex: (this.state.slideIndex + slideLength + step) % slideLength
         });
+    },
+    render: function() {
+        var leftButton = null;
+        var rightButton = null;
+
+        if (this.state.slideIndex !== 0) {
+            leftButton = (
+                <span className="NavButton Left" onClick={this.handleIndexChange.bind(this, -1)}></span>
+            );
+        }
+
+        if (this.state.slideIndex !== this.props.slides.length - 1) {
+            rightButton = (
+                <span className="NavButton Right" onClick={this.handleIndexChange.bind(this, 1)}></span>
+            );
+        }
         return (
             <div className="HeroStream">
                 <h2 className="StreamTitle">{this.props.name}</h2>
                 <HeroVideo {...this.props.heroVideo}/>
-                {heroSlides}
-                <div className="HeroNavButtonContainer">
-                    <span className="NavButton Left"></span>
-                    <span className="NavButton Right"></span>
-                </div>
+                <ReactCSSTransitionGroup transitionName="example">
+                    <HeroSlide videos={this.props.slides[this.state.slideIndex]}/>
+                </ReactCSSTransitionGroup>
+                <div className="HeroNavButtonContainer">{leftButton}{rightButton}</div>
             </div>
         );
     }
