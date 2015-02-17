244a90a6362a1d81f7fe109ffc60a7ba449226d7
Jason Huang <cjhuang@yahoo-inc.com>
2015-02-17 17:00:27 +0800

    reactify HeroVideo

--- components/HeroStream.jsx
+++ components/HeroStream.jsx
@@ -1,17 +1,24 @@
 'use strict';
 
 var React = require('react');
+var HeroVideo = require('./HeroVideo');
 var CoverTile = require('./CoverTile');
 
 var HeroStream = React.createClass({
+    getDefaultProps: function() {
+        return {
+            name: 'Now Watching',
+            heroVideo: {
+                youtubeId: '5XYxuVYmR6A'
+            },
+            slides: [1],
+            initSlideIndex: 0
+        };
+    },
     render: function() {
-        return (
-            <div className="HeroStream">
-                <h2 className="StreamTitle">Now Watching</h2>
-                <div className="HeroVideo">
-                    <img src="http://placehold.it/560x315" alt=""/>
-                </div>
-                <div className="HeroSlide">
+        var heroSlides = this.props.slides.map(function renderHeroSlide(slide, index) {
+            return (
+                <div className="HeroSlide" key={index}>
                     <CoverTile isActive={true} />
                     <CoverTile/>
                     <CoverTile/>
@@ -19,6 +26,13 @@ var HeroStream = React.createClass({
                     <CoverTile/>
                     <CoverTile/>
                 </div>
+            );
+        });
+        return (
+            <div className="HeroStream">
+                <h2 className="StreamTitle">{this.props.name}</h2>
+                <HeroVideo {...this.props.heroVideo}/>
+                {heroSlides}
                 <div className="HeroNavButtonContainer">
                     <span className="NavButton Left"></span>
                     <span className="NavButton Right"></span>
---
+++ components/HeroVideo.jsx
@@ -0,0 +1,28 @@
+'use strict';
+
+var React = require('react');
+
+var HeroVideo = React.createClass({
+    getDefaultProps: function() {
+        return {
+            youtubeId: 'bw9CALKOvAI',
+            shouldAutoplay: false,
+            startAt: ''
+        };
+    },
+    render: function() {
+        var youtubeSrc = 'https://www.youtube.com/embed/' + this.props.youtubeId;
+        
+        if (this.props.shouldAutoplay) {
+            youtubeSrc += '?autoplay=1';
+        }
+
+        return (
+            <div className="HeroVideo">
+                <iframe width="560" height="315" src={youtubeSrc} frameBorder="0" allowFullScreen/>
+            </div>
+        );
+    }
+});
+
+module.exports = HeroVideo;
