176c4e9aa334e100ec7d4e763d499c71c790245f
Jason Huang <cjhuang@yahoo-inc.com>
2015-02-17 15:50:58 +0800

    reactify NavMenu

---
+++ components/PageStream.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+var React = require('react');
+
+var PageStream = React.createClass({
+    render: function() {
+        return (
+            <div>This is a description of the site.</div>
+        );
+    }
+});
+
+module.exports = PageStream;
