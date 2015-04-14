9c7c4ac601deda06966fed87c45ac778a701fe4d
Jason Huang <cjhuang@yahoo-inc.com>
2015-04-14 23:57:38 +0800

    remove unnecessary factory wrapper

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 'use strict';
 
-var React = require('react');
 var Fluxible = require('fluxible');
 var routrPlugin = require('fluxible-plugin-routr');
 
 // create new fluxible instance
 var app = new Fluxible({
-    component: React.createFactory(require('./components/Application.jsx'))
+    component: require('./components/Application.jsx')
 });
 
 // add routes to the routr plugin
