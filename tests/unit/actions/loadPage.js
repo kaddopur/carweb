c828d31b67b08921af547cf05cef6bbd00699afd
Jason Huang <cjhuang@yahoo-inc.com>
2015-04-15 21:45:47 +0800

    remove jest, add mocha and chai

---
+++ tests/unit/actions/loadPage.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('loadPage', function () {
+    it('should fire UPDATE_PAGE_TITLE event', function (done) {
+        expect(1).to.equal(1);
+        
+        done();
+    });
+});
