7bfc04d6c05b062db8218ffbb5565af0e99dafe7
Jason Huang <cjhuang@yahoo-inc.com>
2015-04-15 22:43:29 +0800

    [CARWEB-31] add action test

--- tests/unit/actions/loadPage.js
+++ tests/unit/actions/loadPage.js
@@ -1,11 +1,34 @@
 'use strict';
 
 var expect = require('chai').expect;
+var loadPage = require('../../../actions/loadPage.js');
 
 describe('loadPage', function () {
     it('should fire UPDATE_PAGE_TITLE event', function (done) {
-        expect(1).to.equal(1);
-        
-        done();
+        var payload = {
+            config: {
+                title: 'foo'
+            }
+        };
+
+        var option = {
+            dispatcherContext: {
+                dispatch: function(name, incomingPayload) {
+                    expect(name).to.equal('UPDATE_PAGE_TITLE');
+                    expect(incomingPayload).to.deep.equal({ pageTitle: payload.config.title});
+                }
+            }
+        };
+
+        var mockActionContext = require('fluxible/utils').createMockActionContext(option);
+
+        mockActionContext.executeAction(loadPage, payload, function(err) {
+            if (err) {
+                done(err);
+                return;
+            }
+
+            done();
+        });
     });
 });
