ce780c6f30f4a695e25209b09d44f8862f4521c4
Jason Huang <cjhuang@yahoo-inc.com>
2015-04-18 18:09:57 +0800

    add link to logo

--- components/NavMenu.jsx
+++ components/NavMenu.jsx
@@ -2,11 +2,14 @@
 
 import React from 'react';
 
+// components
+import { NavLink } from 'flux-router-component'
+
 var NavMenu = React.createClass({
     render() {
         return (
             <div className="NavMenu">
-                <a href="#" className="Logo"></a>
+                <NavLink href="/" className="Logo"></NavLink>
                 <ul>
                     <li className="active"><a href="#">Stream</a></li>
                     <li><a href="#">Map Search</a></li>
