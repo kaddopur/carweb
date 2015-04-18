25d8450dc9324e151d9d3463af7ad759df9e7204
Jason Huang <cjhuang@yahoo-inc.com>
2015-04-18 18:34:56 +0800

    move streams into configs

--- components/NavMenu.jsx
+++ components/NavMenu.jsx
@@ -5,42 +5,27 @@ import React from 'react';
 // components
 import { NavLink } from 'flux-router-component';
 import classNames from 'classnames';
+import streams from '../configs/streams'
 
 var NavMenu = React.createClass({
     propTypes: {
         route: React.PropTypes.object.isRequired
     },
 
-    getDefaultProps: function() {
-        return {
-            navItems: [
-                {
-                    path: '/stream/newest',
-                    alias: '/',
-                    title: '最新影片'
-                },
-                {
-                    path: '/stream/reported',
-                    title: '新聞報導'
-                }
-            ]
-        };
-    },
-
-    isActiveLink(navItem) {
-        return this.props.route.url === navItem.path ||
-               this.props.route.url === navItem.alias;
+    isActiveLink(streamItem) {
+        return this.props.route.url === streamItem.path ||
+               this.props.route.url === streamItem.alias;
     },
 
     render() {
-        var links = this.props.navItems.map(navItem => {
+        var links = streams.map(streamItem => {
             var listClass = classNames({
-                'active': this.isActiveLink(navItem)
+                'active': this.isActiveLink(streamItem)
             });
 
             return (
                 <li className={listClass}>
-                    <NavLink href={navItem.path}>{navItem.title}</NavLink>
+                    <NavLink href={streamItem.path}>{streamItem.title}</NavLink>
                 </li>
             );
         }, this);
---
+++ configs/streams.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var streams = [
+    {
+        path: '/stream/newest',
+        alias: '/',
+        title: '最新影片'
+    },
+    {
+        path: '/stream/reported',
+        title: '新聞報導'
+    }
+];
+
+export default streams;
