a436d89558059eb8cd21487f1c04a2c9f944b390
Jason Huang <cjhuang@yahoo-inc.com>
2015-04-19 17:50:37 +0800

    add immutable streamsMock

--- components/PageStream.jsx
+++ components/PageStream.jsx
@@ -3,11 +3,60 @@
 var debug = require('debug')('Components:PageStream');
 
 import React from 'react';
+import Immutable from 'immutable';
 import streamsConfig from '../configs/streams';
 
 // components
 import HeroStream from './HeroStream';
 import Stream from './Stream';
+
+var streamsMock = Immutable.fromJS({
+    newest: {
+        title: streamsConfig.newest.title,
+        videos: [
+            {
+                youtubeId: '5XYxuVYmR6A'
+            },
+            {
+                youtubeId: 'lOGAI_lTyJI'
+            },
+            {
+                youtubeId: 'dFf4AgBNR1E'
+            },
+            {
+                youtubeId: 'teMdjJ3w9iM'
+            },
+            {
+                youtubeId: 'qEYOyZVWlzs'
+            }
+        ]
+    },
+    reported: {
+        title: streamsConfig.reported.title,
+        videos: [
+            {
+                youtubeId: '5XYxuVYmR6A'
+            },
+            {
+                youtubeId: 'lOGAI_lTyJI'
+            }
+        ]
+    },
+    reckless: {
+        title: streamsConfig.reckless.title,
+        videos: [
+            {
+                youtubeId: '5XYxuVYmR6A'
+            },
+            {
+                youtubeId: 'lOGAI_lTyJI'
+            },
+            {
+                youtubeId: 'qEYOyZVWlzs'
+            }
+        ]
+    }
+})
 
 var HERO_STREAM_MOCK = {
     name: 'Car collision',
@@ -33,6 +82,8 @@ var HERO_STREAM_MOCK = {
 var PageStream = React.createClass({
     render() {
         debug('rendered');
+
+        debug(streamsMock.get('newest'));
 
         return (
             <div className="PageStream">
--- configs/streams.js
+++ configs/streams.js
@@ -9,6 +9,10 @@ var streams = {
     reported: {
         path: '/stream/reported',
         title: '新聞報導'
+    },
+    reckless: {
+        path: '/stream/reckless',
+        title: '危險駕駛'
     }
 };
 
