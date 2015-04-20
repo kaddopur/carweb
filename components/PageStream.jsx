3fb78d3c3fe7add46f15c47126a8d55d0219951a
Jason Huang <cjhuang@yahoo-inc.com>
2015-04-21 00:37:43 +0800

    add StreamStore

--- components/PageStream.jsx
+++ components/PageStream.jsx
@@ -3,113 +3,12 @@
 import React from 'react';
 import Immutable from 'immutable';
 import { ComponentMixin as ImmutableMixin } from 'fluxible-immutable-utils';
-import streamsConfig from '../configs/streams';
 
 // components
 import HeroStream from './HeroStream';
 import Stream from './Stream';
 
-var streamsMock = Immutable.fromJS({
-    newest: {
-        name: streamsConfig.newest.name,
-        title: streamsConfig.newest.title,
-        videos: [
-            {
-                youtubeId: '5XYxuVYmR6A'
-            },
-            {
-                youtubeId: 'lOGAI_lTyJI'
-            },
-            {
-                youtubeId: 'dFf4AgBNR1E'
-            },
-            {
-                youtubeId: 'teMdjJ3w9iM'
-            },
-            {
-                youtubeId: 'qEYOyZVWlzs'
-            }
-        ]
-    },
-    reported: {
-        name: streamsConfig.reported.name,
-        title: streamsConfig.reported.title,
-        videos: [
-            {
-                youtubeId: '5XYxuVYmR6A'
-            },
-            {
-                youtubeId: 'lOGAI_lTyJI'
-            }
-        ]
-    },
-    reckless: {
-        name: streamsConfig.reckless.name,
-        title: streamsConfig.reckless.title,
-        videos: [
-            {
-                youtubeId: 'dFf4AgBNR1E'
-            },
-            {
-                youtubeId: 'teMdjJ3w9iM'
-            },
-            {
-                youtubeId: 'qEYOyZVWlzs'
-            }
-        ]
-    },
-    exid: {
-        name: streamsConfig.exid.name,
-        title: streamsConfig.exid.title,
-        videos: [
-            {
-                youtubeId: 'OTyJkMK8m_o'
-            },
-            {
-                youtubeId: 'ws5Jvmonq-w'
-            },
-            {
-                youtubeId: 'NNxerm8EdPo'
-            },
-            {
-                youtubeId: 'hfXZ6ydgZyo'
-            },
-            {
-                youtubeId: 'egqDPipqIAg'
-            }
-        ]
-    },
-    redVelvet: {
-        name: streamsConfig.redVelvet.name,
-        title: streamsConfig.redVelvet.title,
-        videos: [
-            {
-                youtubeId: 'JFgv8bKfxEs'
-            },
-            {
-                youtubeId: 'QpAn9ryoB4Y'
-            },
-            {
-                youtubeId: 'glXgSSOKlls'
-            },
-            {
-                youtubeId: 'px2Q47O0_eE'
-            }
-        ]
-    },
-    lovelyz: {
-        name: streamsConfig.lovelyz.name,
-        title: streamsConfig.lovelyz.title,
-        videos: [
-            {
-                youtubeId: 'Mn0ToZ9ki3Q'
-            },
-            {
-                youtubeId: 'Bfejxue4vMs'
-            }
-        ]
-    }
-});
+var streamsMock = Immutable.Map();
 
 var PageStream = React.createClass({
     mixins: [ ImmutableMixin ],
---
+++ stores/StreamStore.js
@@ -0,0 +1,122 @@
+'use strict';
+
+import { createImmutableStore } from 'fluxible-immutable-utils';
+import streamsConfig from '../configs/streams';
+
+var StreamStore = createImmutableStore({
+    storeName: 'StreamStore',
+
+    handlers: {
+        READ_STREAMS_SUCCESS: '_onReadStreams'
+    },
+
+    getStreams() {
+        return this._state.get('streams');
+    },
+
+    _onReadStreams() {
+        this.setState(this._state.set('streams', {
+            newest: {
+                name: streamsConfig.newest.name,
+                title: streamsConfig.newest.title,
+                videos: [
+                    {
+                        youtubeId: '5XYxuVYmR6A'
+                    },
+                    {
+                        youtubeId: 'lOGAI_lTyJI'
+                    },
+                    {
+                        youtubeId: 'dFf4AgBNR1E'
+                    },
+                    {
+                        youtubeId: 'teMdjJ3w9iM'
+                    },
+                    {
+                        youtubeId: 'qEYOyZVWlzs'
+                    }
+                ]
+            },
+            reported: {
+                name: streamsConfig.reported.name,
+                title: streamsConfig.reported.title,
+                videos: [
+                    {
+                        youtubeId: '5XYxuVYmR6A'
+                    },
+                    {
+                        youtubeId: 'lOGAI_lTyJI'
+                    }
+                ]
+            },
+            reckless: {
+                name: streamsConfig.reckless.name,
+                title: streamsConfig.reckless.title,
+                videos: [
+                    {
+                        youtubeId: 'dFf4AgBNR1E'
+                    },
+                    {
+                        youtubeId: 'teMdjJ3w9iM'
+                    },
+                    {
+                        youtubeId: 'qEYOyZVWlzs'
+                    }
+                ]
+            },
+            exid: {
+                name: streamsConfig.exid.name,
+                title: streamsConfig.exid.title,
+                videos: [
+                    {
+                        youtubeId: 'OTyJkMK8m_o'
+                    },
+                    {
+                        youtubeId: 'ws5Jvmonq-w'
+                    },
+                    {
+                        youtubeId: 'NNxerm8EdPo'
+                    },
+                    {
+                        youtubeId: 'hfXZ6ydgZyo'
+                    },
+                    {
+                        youtubeId: 'egqDPipqIAg'
+                    }
+                ]
+            },
+            redVelvet: {
+                name: streamsConfig.redVelvet.name,
+                title: streamsConfig.redVelvet.title,
+                videos: [
+                    {
+                        youtubeId: 'JFgv8bKfxEs'
+                    },
+                    {
+                        youtubeId: 'QpAn9ryoB4Y'
+                    },
+                    {
+                        youtubeId: 'glXgSSOKlls'
+                    },
+                    {
+                        youtubeId: 'px2Q47O0_eE'
+                    }
+                ]
+            },
+            lovelyz: {
+                name: streamsConfig.lovelyz.name,
+                title: streamsConfig.lovelyz.title,
+                videos: [
+                    {
+                        youtubeId: 'Mn0ToZ9ki3Q'
+                    },
+                    {
+                        youtubeId: 'Bfejxue4vMs'
+                    }
+                ]
+            }
+        }));
+    }
+});
+
+export default StreamStore;
